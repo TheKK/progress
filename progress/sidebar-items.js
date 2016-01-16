bff768f94a4b82b8e28b9a27523278e2ce60b3d7
Ying-Ruei Liang (KK) <thumbd03803@gmail.com>
2016-01-16 19:14:12 +0800

    Update document

--- progress/sidebar-items.js
+++ progress/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Bar","Struct that used for presenting progress bar with plain texts."]]});
+initSidebarItems({"struct":[["Bar","Struct that used for presenting progress bar with plain texts."],["BarBuilder","A builder that used for creating customize progress bar."],["SpinningCircle","Struct that used for presenting progress with plain texts."],["Text","Struct that used for presenting progress with plain texts."]]});
